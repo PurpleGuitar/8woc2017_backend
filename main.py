9994c064c437373375557042773e4318b528fc4b
Craig Oliver <craig.robert.oliver@gmail.com>
2017-10-20 11:56:22 -0400

    Improve error handling

--- main.py
+++ main.py
@@ -20,7 +20,6 @@ def main():
 
 class MyHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
 
-
     def do_GET(self):
 
         # Initialize response
@@ -29,6 +28,7 @@ class MyHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         data["chapter"] = ""
         data["verse_num"] = ""
         data["verse"] = ""
+        data["error"] = "false"
         data["message"] = ""
 
         # Check path against expected pattern
@@ -44,6 +44,7 @@ class MyHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             response = lookup_verse_from_ulb(book, chapter, verse_num)
             if response["error"]:
                 self.send_response(404)
+                data["error"] = "true"
                 data["message"] = response["message"]
             else:
                 data["verse"] = response["verse"]
@@ -51,6 +52,7 @@ class MyHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         else:
             # Not sure what client is looking for
             self.send_response(404)
+            data["error"] = "true"
             data["message"] = "Couldn't understand path '"+self.path+"', expecting e.g. /verses/gen/1/1"
 
         # Send data response
@@ -60,28 +62,44 @@ class MyHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         self.wfile.write(bytes(json.dumps(data), "utf8"))
 
 def lookup_verse_from_ulb(book, chapter, verse_num):
+
+    # Prepare response
     response = {}
     response["verse"] = ""
     response["error"] = False
     response["message"] = ""
+
+    # Look in zip file for book
     zf = zipfile.ZipFile('ulb.zip')
     filename_match_regex = re.compile(FILENAME_PREFIX_REGEX + book.upper() + FILENAME_SUFFIX_REGEX)
     filenames = zf.namelist()
     for filename in filenames:
         match = filename_match_regex.match(filename)
         if match:
+            # Found book, lookup chapter and verse
             usfm = zf.read(filename)
             return lookup_verse_from_usfm(usfm, chapter, verse_num)
+
     # Couldn't find match, return blank
     response["error"] = True
     response["message"] = "Couldn't find book named " + book
     return response
 
 def lookup_verse_from_usfm(usfm, chapter, verse_num):
+
+    # Prepare response
     response = {}
-    response["verse"] = "OK" #FIXME
+    response["verse"] = ""
     response["error"] = False
     response["message"] = ""
+
+    # Look for chapter and verse
+    chapter_matches = False
+    for line in usfm:
+        print(line)
+
+
+    # Finished
     return response
 
 
