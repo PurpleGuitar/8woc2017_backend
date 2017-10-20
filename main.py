0eabbd5012ef58a9c2a883ee6485fdc5c81a527a
Craig Oliver <craig.robert.oliver@gmail.com>
2017-10-20 10:26:41 -0400

    Built simple http server

---
+++ main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import http.server
+
+def main():
+    print("Starting server...", end='', flush=True)
+    server_address = ("0.0.0.0", 8000)
+    httpd = http.server.HTTPServer(server_address, MyHTTPRequestHandler)
+    print("done.")
+    print("Server running.")
+    httpd.serve_forever()
+
+class MyHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("hello!", "utf8"))
+
+if __name__ == "__main__":
+    main()
